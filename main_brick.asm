d8a30dc47c0fff447200411f5f09d4550c2b666f
Loay Ahmed <loayahmed304@gmail.com>
2024-12-04 23:40:14 +0200

    feat: add loop to render bricks rows and columns

--- main_brick.asm
+++ main_brick.asm
@@ -10,15 +10,42 @@ public brick_height
     brick_width dw 32d					;bricks width
     brick_height dw 7d					;bricks height
 .code
-main PROC
-    MOV AX, @data
-    MOV DS, AX
+main PROC FAR
+    mov ax, @data
+    mov ds, ax
+	
+    MOV AL, 13h
+    MOV AH, 0
+    INT 10h								;set video mode
 
-    MOV CX, bricks_initial_x[0]     ;move the initial x value in CX
-    MOV DX, bricks_initial_y[0]     ;move the initial y value in DX
-    call brick                      ;draws only 1 brick, expects the initial x and y to be in CX and DX respectively
-
-    MOV AX, 4c00h
-    INT 21h
+    MOV CX, 8							;counter for number of bricks in each row
+	MOV DI, 0							;index for column
+	MOV SI, 0							;index for row
+	MOV DX, bricks_initial_y			;get first y-value and store in DX
+    
+	; Draw bricks in current row
+	Render_Horizontal:
+        PUSH CX							;store CX to keep for counter
+        ;Store the x-value in CX, and y-value in DX, the PROC Brick expects them that way
+		MOV CX, bricks_initial_x[DI]	;store in CX and initial x of index DI (0 initially)
+		PUSH DI							;store DI to protect index before calling the PROC
+		CALL Brick						;draw a single brick, of initial X at CX, and initial Y in DX
+		POP DI							;restore DI
+		ADD DI, 2						;increment index by 2 (because its word)
+        POP CX							;restore counter
+    loop Render_Horizontal
+	
+	; When row rendeing loop ends, lets go one new row below it
+	Render_Vertical:
+		MOV DI, 0						;reset initial x index back to 0
+		ADD SI, 2						;increment initial y index
+		CMP SI, 10						;check if we reached the limit of bricks (10)
+		JGE END_PROC					;if we did, return
+		MOV DX, Bricks_Initial_y[SI]	;put the current y by its index in DX
+		mov cx, 8						;restore CX for the upper loop
+		JMP Render_Horizontal			;go back render this row
+	
+	END_PROC:							;end
+	ret
 main ENDP
 end main
