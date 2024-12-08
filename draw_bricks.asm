12189198a28ad6cfb24e3da3d16e57483428abf9
Loay Ahmed <loayahmed304@gmail.com>
2024-12-08 16:07:46 +0200

    Feat/Bricks Drawing (#2)
    * feat: add procedure to draw a single brick
    
    * feat: add loop to render bricks rows and columns
    
    * feat: color brick rows
    
    * refactor: rename procedure to match its functionality
    
    * refactor: store bricks number in variable
    
    * refactor: delete redundant stack operation
    
    * style: change bricks colors
    
    * refactor: make rows count and columns count constants

---
+++ draw_bricks.asm
@@ -0,0 +1,57 @@
+
+EXTRN Brick:FAR
+public brick_width
+public brick_height
+
+.model small
+.stack 64
+.data
+    bricks_initial_x dw 4d, 43d, 82d, 121d, 160d, 199d, 238d, 277d		;initial x-values (columns) for bricks (total=8)
+    bricks_initial_y dw 6d, 20d, 34d, 48d, 62d							;initial y-values (rows) for bricks (total=5)
+	colors dw 7, 10, 9, 13, 12											;colors for rows (grey, green, blue, pink, red)
+    brick_width dw 32d					;bricks width
+    brick_height dw 9d					;bricks height
+	num_columns EQU 8					;number of bricks in each row
+	num_rows EQU 5						;number of rows
+.code
+draw_bricks PROC FAR
+    mov ax, @data
+    mov ds, ax
+	
+    MOV AL, 13h
+    MOV AH, 0
+    INT 10h								;set video mode
+
+    MOV CX, num_columns					;counter for number of bricks in each row
+	MOV DI, 0							;index for column
+	MOV SI, 0							;index for row
+	MOV DX, bricks_initial_y			;get first y-value and store in DX
+    
+	; Draw bricks in current row
+	Render_Horizontal:
+        PUSH CX							;store CX to keep for counter
+		
+        ;Store the x-value in CX, and y-value in DX, the PROC Brick expects them that way
+		MOV AX, colors[SI]				;set row color according to index (we're only interested in AL)
+		MOV CX, bricks_initial_x[DI]	;store in CX and initial x of index DI (0 initially)
+		PUSH DI							;store DI to protect index before calling the PROC
+		CALL Brick						;draw a single brick, of initial X at CX, and initial Y in DX
+		POP DI							;restore DI
+		ADD DI, 2						;increment index by 2 (because its word)
+        POP CX							;restore counter
+    loop Render_Horizontal
+	
+	; When row rendeing loop ends, lets go one new row below it
+	Render_Vertical:
+		MOV DI, 0						;reset initial x index back to 0
+		ADD SI, 2						;increment initial y index
+		CMP SI, num_rows * 2			;check if we reached the limit of bricks (10)
+		JGE END_PROC					;if we did, return
+		MOV DX, Bricks_Initial_y[SI]	;put the current y by its index in DX
+		mov cx, 8						;restore CX for the upper loop
+		JMP Render_Horizontal			;go back render this row
+	
+	END_PROC:							;end
+	ret
+draw_bricks ENDP
+end draw_bricks
